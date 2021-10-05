a1a231f84ef154654a6dc76d456fd37478d5e623
Rakib Ali <r.ali@se18.qmul.ac.uk>
2021-10-05 18:09:34 +0100

    HeaderCartButton

--- food-delivery/src/components/Layout/Header.js
+++ food-delivery/src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 export default function Header() {
   return (
     <>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton>Cart</HeaderCartButton>
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of food" />
