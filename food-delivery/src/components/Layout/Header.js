cfda068a0d9ee6a03bdd5243afb935a44a551632
Rakib Ali <r.ali@se18.qmul.ac.uk>
2021-10-05 18:22:40 +0100

    Meal Summary

--- food-delivery/src/components/Layout/Header.js
+++ food-delivery/src/components/Layout/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
     <>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <HeaderCartButton>Cart</HeaderCartButton>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of food" />
