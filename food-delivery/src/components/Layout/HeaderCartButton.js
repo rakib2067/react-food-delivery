3167939a4945121f95ff58e5048ad837a6ec08e2
Rakib Ali <r.ali@se18.qmul.ac.uk>
2021-10-12 16:24:21 +0100

    Added cart animation

--- food-delivery/src/components/Layout/HeaderCartButton.js
+++ food-delivery/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import cart_context from "../../Context/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 export default function HeaderCartButton(props) {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartContext = useContext(cart_context);
   const numOfItems = cartContext.items.reduce((currNum, item) => {
     return currNum + item.amount;
   }, 0);
+  const { items } = cartContext;
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button onClick={props.onClick} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
